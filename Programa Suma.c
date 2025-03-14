c4aa2976aa15e9491b952dcd3a5f3705c017a7d6
Pablix44737 <pabloluna44737@gmail.com>
2025-03-14 15:58:49 -0300

    Subiendo programa de suma en C y el gitignore

---
+++ Programa Suma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,suma;
+    printf("Digite el primer numero: ");
+    scanf("%i",&a);
+    printf("Digite el segundo numero: ");
+    scanf("%i",&b);
+    suma = a + b;
+    printf("La suma es: %i",suma);
+    
+    return 0;
+}
